4d31dceeabd08cf3730a929fc64b4508959542f8
Derek Nowicki <derek@knowikiapps.com>
2021-04-26 20:57:27 -0700

    add summary page and form submission action

--- thelazyreader.pl
+++ thelazyreader.pl
@@ -3,6 +3,7 @@
 :- use_module(library(http/html_write)).
 
 :- http_handler(root(thelazyreader), homepage, []).
+:- http_handler('/summary', summarypage, []).
 
 server(Port) :-
   http_server(http_dispatch, [port(Port)]).
@@ -19,7 +20,7 @@ homepage(_Request) :-
       '<body>',
         '<h1>', 'The Lazy Reader', '</h1>',
         '<p>', 'Summarize any text!', '</p>',
-        '<form id="articleform">',
+        '<form id="articleform" action="/summary">',
           '<textarea rows="4" cols="50">', 'Copy your text here...','</textarea>',
           '<br>','<br>',
           '<input type = "submit" value = "Summarize" />',
@@ -27,3 +28,20 @@ homepage(_Request) :-
       '</body>',
     '</html>'
   ]).
+
+  summarypage(_Request) :-
+    format('Content-type: text/html~n~n'),
+    print_html([
+    '<html>',
+      '<head>',
+        '<title>',
+          'The Lazy Reader',
+        '</title>',
+      '</head>',
+      '<body>',
+        '<h1>', 'Text Summary', '</h1>',
+        '<p>', 'This is where the summary will go!', '</p>',
+        '<a href="/thelazyreader">', 'Summarize again', '</a>',
+      '</body>',
+    '</html>'
+    ]).
