ccdbb1e685c04e94f3663b4674b290ed6d350d77
danielyoon-maker <58200952+danielyoon-maker@users.noreply.github.com>
2021-05-21 09:28:30 -0700

    update algorithm to weigh proper nouns by themselves

--- look_up_list.pl
+++ look_up_list.pl
@@ -34,7 +34,7 @@ lookupList([X|T],ListOfGloss) :-
     add_head(ListOfGloss1,TokenizedListofGloss,ListOfGloss).  
 
 errorHandler(WordNotFound, DummyOutput) :- 
-    DummyOutput = 'NOT FOUND'.
+    DummyOutput = WordNotFound.
 
 add_head([],X,[X]).
 add_head(List, Item, [Item|List]).
